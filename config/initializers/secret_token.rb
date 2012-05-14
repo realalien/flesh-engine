e2717664252ce0faaf1e863fbca6dc8cf8a8c19c
realalien <realalien@gmail.com>
2012-05-14 10:57:14 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FleshEngine::Application.config.secret_token = 'ddc5c2eb4d534dcd7fbee1834574c0a628d070aa5a096d46efd37b8afca93f57f2ca46e9dd0591b86833f5bd4c8d53c485e79b93c8e58e56846fe1b318a7bbe3'
