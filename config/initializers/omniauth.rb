1a1ef8dd177e1023c6c36ff00de91f50bccfa41e
realalien <realalien@gmail.com>
2012-05-22 18:24:15 +0800

    daily update, no progress

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 require 'oa-oauth'
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :tsina, '3422703718', '56b821f7588cf3987878b7d5beed1c32' #ENV['WEIBO_KEY'], ENV['WEIBO_SECRET']
+  provider OmniAuth::Strategies::Tsina, '3422703718', '56b821f7588cf3987878b7d5beed1c32' #ENV['WEIBO_KEY'], ENV['WEIBO_SECRET']
 end
